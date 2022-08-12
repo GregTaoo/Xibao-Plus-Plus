41ecc5bae081cd6a7d4a24e90e67021d0754804e
GregTaoo <gregtaoo@outlook.com>
2022-08-12 22:44:54 +0800

    second

--- Fabric-1.19/src/main/java/top/gregtao/xibaopp/XibaoPlusPlusMusic.java
+++ Fabric-1.19/src/main/java/top/gregtao/xibaopp/XibaoPlusPlusMusic.java
@@ -6,5 +6,5 @@ import net.minecraft.util.Identifier;
 
 public class XibaoPlusPlusMusic {
     public static SoundEvent XIBAO_SOUND_EVENT = new SoundEvent(new Identifier("xibaopp", "xibao"));
-    public static MusicSound XIBAO_MUSIC = new MusicSound(XIBAO_SOUND_EVENT, 10, 40, true);
+    public static MusicSound XIBAO_MUSIC = new MusicSound(XIBAO_SOUND_EVENT, 0, 0, true);
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+## 自定义音乐/图片方式
+### 使用资源包
+路径：
+- assets/xibaopp/sounds
+- assets/xibaopp/textures
